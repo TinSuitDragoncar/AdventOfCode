46367718aa5b4fc6859cb46b52788c24a323a8dd
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2021-12-05 19:32:10 +0000

    Day 3 Part 1 Solution

--- AdventOfCode/Day3/Day3.cs
+++ AdventOfCode/Day3/Day3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,8 +14,36 @@ namespace AdventOfCode
         {
             List<string> lines = File.ReadAllLines(@"Day3/input.txt").ToList();
 
+            List<int> oneCount = new List<int>(new int[lines.First().Length]);
 
-            Console.WriteLine("Day 3 Part 1: ");
+            foreach (string l in lines)
+            {
+                for(int i = 0; i < l.Length; ++i)
+                {
+                    if (l[i] == '1')
+                    {
+                        ++oneCount[i];
+                    }
+                }
+            }
+
+            int gamma = 0;
+            int epsilon = 0;
+            float half = lines.Count / 2;
+            for (int i = 0; i < oneCount.Count; ++i)
+            {
+                int valToAdd = (int) Math.Pow(2, (oneCount.Count - i - 1));
+                if (oneCount[i] > half)
+                {
+                    gamma += valToAdd;
+                }
+                else
+                {
+                    epsilon += valToAdd;
+                }
+            }
+
+            Console.WriteLine("Day 3 Part 1: Power Consumption is Gamma ({0}) * Epsilon ({1}) = {2}", gamma, epsilon, gamma * epsilon);
         }
     }
 }
