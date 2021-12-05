15887f6df19ccd5f55281ab4f6ac05095f875107
TinSuitDragoncar <fergus.brown123@gmail.com>
2021-12-05 17:47:44 +0000

    Day 2 Part 1 Solution

---
+++ AdventOfCode/Day2/Day2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day2
+    {
+        public static void Part1()
+        {
+            List<string> lines = File.ReadAllLines(@"Day2/input.txt").ToList();
+            int horizontalPos = 0;
+            int depth = 0;
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(' ');
+                string direction = values[0];
+                int distance = Int32.Parse(values[1]);
+                switch (direction)
+                {
+                    case "forward":
+                        horizontalPos += distance;
+                        break;
+
+                    case "up":
+                        depth -= distance;
+                        break;
+
+                    case "down":
+                        depth += distance;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Day 2 Part 1: Depth ({0}) * Horizontal Position ({1}) = {2}", depth, horizontalPos, horizontalPos * depth);
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,6 +11,8 @@ namespace AdventOfCode
         {
             Day1.Part1();
             Day1.Part2();
+            Day2.Part1();
+            Day2.Part2();
         }
     }
 }
