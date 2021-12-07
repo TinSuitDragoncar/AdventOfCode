18c376af6385a6a9579319706630f9a937eb374c
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2021-12-07 22:27:06 +0000

    Day 7 Part 2 Solution

--- AdventOfCode/Day7/Day7.cs
+++ AdventOfCode/Day7/Day7.cs
@@ -37,5 +37,35 @@ namespace AdventOfCode
 
             Console.WriteLine("Day 7 Part 1: The crabs should align to position {0} consuming {1} fuel", bestPosition, minFuel);
         }
+
+        public static void Part2()
+        {
+            List<int> crabPositions = File.ReadAllLines(@"Day7/input.txt").First().Split(',').Select(x => Int32.Parse(x)).ToList();
+
+            int min = crabPositions.Min();
+            int max = crabPositions.Max();
+
+            List<int> endPositions = Enumerable.Range(min, max - min).ToList();
+
+            int bestPosition = 0;
+            int minFuel = Int32.MaxValue;
+
+            foreach (int pos in endPositions)
+            {
+                int fuel = 0;
+                foreach (int crab in crabPositions)
+                {
+                    int diff = Math.Abs(crab - pos);
+                    fuel += (diff * (diff + 1)) / 2;
+                }
+                if (fuel < minFuel)
+                {
+                    bestPosition = pos;
+                    minFuel = fuel;
+                }
+            }
+
+            Console.WriteLine("Day 7 Part 2: The crabs should align to position {0} consuming {1} fuel", bestPosition, minFuel);
+        }
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day7.Part1();
+            Day7.Part2();
         }
     }
 }
