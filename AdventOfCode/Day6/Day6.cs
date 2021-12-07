5532208734b94d27557a4d8122b7d05fc3c7a710
TinSuitDragoncar <fergus.brown123@gmail.com>
2021-12-07 10:21:30 +0000

    Day 6 Part 1

--- AdventOfCode/Day6/Day6.cs
+++ AdventOfCode/Day6/Day6.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,40 @@ namespace AdventOfCode
 {
     class Day6
     {
+        public static void Part1()
+        {
+            List<int> initialState = File.ReadAllLines(@"Day6/test.txt").First().Split(',').Select(x => Int32.Parse(x)).ToList();
+
+            int count = initialState.Count;
+            const int days = 18;
+            foreach (int state in initialState)
+            {
+                int dayOffset = 8 - state;
+
+                count += GetProduction(days + dayOffset);
+            }
+
+            Console.WriteLine("Day 6 Part 1: Total number of fish after {0} days is {1} ", count);
+        }
+        
+        private static int GetProduction(int days)
+        {
+            const int initialLifespan = 9;
+            const int defaultLifespan = 7;
+            if (initialLifespan > days)
+            {
+                return 0;
+            }
+            int localProduction = 1;
+            days -= initialLifespan;
+            localProduction += days / defaultLifespan;
+            int totalProduction = GetProduction(days) + localProduction;
+            for (int i = 0; i < localProduction - 1; ++i)
+            {
+                days -= defaultLifespan;
+                totalProduction += GetProduction(days);
+            }
+            return totalProduction;
+        }
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -17,8 +17,9 @@ namespace AdventOfCode
             //Day3.Part2();
             //Day4.Part1();
             //Day4.Part2();
-            Day5.Part1();
-            Day5.Part2();
+            //Day5.Part1();
+            //Day5.Part2();
+            Day6.Part1();
         }
     }
 }
