ee4b65e030412e91649713c0362cfa94fef02041
TinSuitDragoncar <fergus.brown123@gmail.com>
2021-12-09 10:18:19 +0000

    Day 9 Part 2

--- AdventOfCode/Day9/Day9.cs
+++ AdventOfCode/Day9/Day9.cs
@@ -63,5 +63,68 @@ namespace AdventOfCode
 
             Console.WriteLine("Day 9 Part 1: danger count is {0}", sum);
         }
+
+        public static void Part2()
+        {
+            List<string> lines = File.ReadAllLines(@"Day9/test.txt").ToList();
+
+            int width = lines.First().Length;
+            int height = lines.Count;
+
+            List<int> input = lines.SelectMany(x => x.ToArray()).Select(y => Int32.Parse(y.ToString())).ToList();
+            HashSet<int> visitedIndices = new HashSet<int>();
+            List<int> basins = new List<int>();
+
+            int sum = 0;
+            for (int i = 0; i < input.Count; ++i)
+            {
+                int basinSize = GetBasinSize(i, -1, input, visitedIndices, width, height);
+                basins.Add(basinSize);
+            }
+
+            Console.WriteLine("Day 9 Part 2: The 3 largest basins are {0}, {1} and {2} with a product of {3}", sum);
+        }
+
+        private static int GetBasinSize(int i, int prevValue, List<int> input, HashSet<int> visitedIndices, int width, int height)
+        {
+            
+            if (input[i] == 9 || visitedIndices.Contains(i))
+            {
+                return 0;
+            }
+            visitedIndices.Add(i);
+
+            int columnIndex = i % width;
+            int rowIndex = i / width;
+
+            int basinCount = 1;
+
+            if (input[i] > prevValue)
+            {
+                if (columnIndex > 0)
+                {
+                    basinCount += GetBasinSize(i - 1, input[i], input, visitedIndices, width, height);
+                }
+                if (columnIndex < width - 1)
+                {
+                    basinCount += GetBasinSize(i + 1, input[i], input, visitedIndices, width, height);
+                }
+                if (rowIndex > 0)
+                {
+
+                    basinCount += GetBasinSize(i - width, input[i], input, visitedIndices, width, height);
+                }
+                if (rowIndex < height - 1)
+                {
+                    basinCount += GetBasinSize(i + width, input[i], input, visitedIndices, width, height);
+                }
+
+                return basinCount;
+            }   
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day9.Part1();
+            Day9.Part2();
         }
     }
 }
