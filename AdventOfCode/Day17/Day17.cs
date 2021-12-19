24923e6a9aba0e609821f357c8982fab45054bbb
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2021-12-19 22:06:54 +0000

    Day 17 Start

---
+++ AdventOfCode/Day17/Day17.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day17
+    {
+        public static void Solve()
+        {
+            string input = File.ReadLines(@"Day17/test.txt").First();
+
+            Match xM = Regex.Match(input, "x=(\d+)..(\d+)");
+            Match yM = Regex.Match(input, "y=(\d+)..(\d+)");
+            (int a, int b) xLims = (Int32.Parse(xM.Groups[1].ToString()), Int32.Parse(xM.Groups[2].ToString()));
+            (int a, int b) yLims = (Int32.Parse(yM.Groups[1].ToString()), Int32.Parse(yM.Groups[2].ToString()));
+
+            int xMin = 
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day16.Solve();
+            Day17.Solve();
         }
     }
 }
