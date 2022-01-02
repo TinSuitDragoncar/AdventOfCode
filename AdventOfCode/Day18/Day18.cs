3eaed677afd77ce835aff4530656d33a94da4be3
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2022-01-02 23:01:07 +0000

    Day 18 Part 1 Solution

--- AdventOfCode/Day18/Day18.cs
+++ AdventOfCode/Day18/Day18.cs
@@ -12,153 +12,167 @@ namespace AdventOfCode
     {
         public static void Solve()
         {
-            string[] snailNumbers = File.ReadAllLines(@"Day18/test.txt");
+           string[] snailNumbers = File.ReadAllLines(@"Day18/input.txt");
 
-            List<Node> nAsTrees = snailNumbers.Select(s => ConvertToTree(s)).ToList();
-
-            Node currentNumber = nAsTrees.First(); 
-            foreach(Node n in nAsTrees.Skip(1))
+           string currentNumber = snailNumbers.First(); 
+            foreach(string n in snailNumbers.Skip(1))
             {
                 currentNumber = AddSnailNumbers(currentNumber, n);
             }
 
-            Console.WriteLine("Result is:\n{0}", currentNumber);
+            int magnitude = FindMagnitude(currentNumber);
+
+            Console.WriteLine("Part 1 Result is:\n{0}", currentNumber);
+            Console.WriteLine("Magnitude is: {0}", magnitude);
+
+            // Part 2
         }
 
-        public class Node
+        private static int FindMagnitude(string s)
         {
-            public Node Parent { get; set; }
-            public int? Data { get; set; }
-            public Node A { get; set; }
-            public Node B { get; set; }
-            public Node(Node parent = null, int? data = null)
-            {
-                Parent = parent;
-                Data = data;
-                A = null;
-                B = null;
-            }
-        }
+            // trim outer brackets
+            s = s.Substring(1, s.Length - 2);
+            int magnitude = 0;
 
-
-        private static Node ConvertToTree(string s)
-        {
-            Node n = new();
-            Match m = Regex.Match(s, @"^\[(\d+),(\d+)\]$");
+            // check both vals
+            Match m = Regex.Match(s, @"^(\d+),(\d+)$");
             if (m.Success)
             {
-                n.A = new(n, Int32.Parse(m.Groups[1].ToString()));
-                n.B = new(n, Int32.Parse(m.Groups[2].ToString()));
+                magnitude += 3 * Int32.Parse(m.Groups[1].ToString());
+                magnitude += 2 * Int32.Parse(m.Groups[2].ToString());
+                return magnitude;
             }
-            else
+            // check left val
+            m = Regex.Match(s, @"^(\d+),");
+            if (m.Success)
             {
-                s = s.Substring(1, s.Length - 2);
-                int balance = 0;
-                for(int i = 0; i < s.Length; i++)
+                magnitude += 3 * Int32.Parse(m.Groups[1].ToString());
+                magnitude += 2 * FindMagnitude(s.Substring(m.Groups[0].Length));
+                return magnitude;
+            }
+            // check right val
+            m = Regex.Match(s, @",(\d+)$");
+            if (m.Success)
+            {
+                magnitude += 3 * FindMagnitude(s.Substring(0, m.Groups[0].Index));
+                magnitude += 2 * Int32.Parse(m.Groups[1].ToString());
+                return magnitude;
+            }
+
+            // otherwise balance brackets to determine where to split the string
+            int balance = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '[')
                 {
-                    if (s[i] == '[')
-                    {
-                        balance++;
-                    }
-                    else if (s[i] == ']')
-                    {
-                        balance--;
-                    }
-                    if (balance == 0)
-                    {
-                        string aString = s.Substring(0, i + 1);
-                        string bString = s.Substring(i + 2, s.Length - i - 2);
-                        int result;
-                        if (Int32.TryParse(aString, out result))
-                        {
-                            n.A = new(n, result);
-                        }
-                        else
-                        {
-                            n.A = ConvertToTree(aString);
-                        }
-                        if (Int32.TryParse(bString, out result))
-                        {
-                            n.B = new(n, result);
-                        }
-                        else
-                        {
-                            n.B = ConvertToTree(bString);
-                        }
-                        break;
-                    }
+                    balance++;
+                }
+                else if (s[i] == ']')
+                {
+                    balance--;
+                }
+                if (balance == 0)
+                {
+                    string lString = s.Substring(0, i + 1);
+                    string rString = s.Substring(i + 2, s.Length - i - 2);
+                    magnitude += 3 * FindMagnitude(lString);
+                    magnitude += 2 * FindMagnitude(rString);
+                    return magnitude;
                 }
             }
 
-            return n;
+            return -1;
         }
 
-        private static Node AddSnailNumbers(Node a, Node b)
+        private static string AddSnailNumbers(string a, string b)
         {
-            Node result = new();
-            result.A = a;
-            result.B = b;
-
-            Console.WriteLine("Attempting to reduce {0}", a);
-            int reduceCount = 0;
-            while (Reduce(result))
+            a = String.Format("[{0},{1}]", a, b);
+            Console.WriteLine("Reducing {0}", a);
+            while (Reduce(a, out string result))
             {
-                reduceCount++;
-                Console.WriteLine("Reduced {0}", reduceCount);
+                a = result;
             }
 
-            return result;
+            return a;
         }
 
-        private static bool Reduce(Node n)
+        private static bool Reduce(string n, out string result)
         {
-            bool result = Explode(n);
-            if (!result)
+            bool bResult = Explode(n, out result);
+            if (!bResult)
             {
-                result = Split(n);
+                bResult = Split(n, out result);
             }
 
-            return result;
+            return bResult;
         }
 
-        private static bool Explode(Node n, int depth = 1)
+        private static bool Explode(string n, out string result)
         {
-            if (depth == 5)
+            int balance = 0;
+            for (int i = 0; i < n.Length; ++i)
             {
-                // EXPLODE LOGIC
-                n.Data = 0;
-                AddLeft(n);
-                AddRight(n);
+                if (n[i] == '[')
+                {
+                    balance++;
+                }
+                else if (n[i] == ']')
+                {
+                    balance--;
+                }
+
+                if (balance == 5)
+                {
+                    Match m = Regex.Match(n.Substring(i), @"\[(\d+),(\d+)\]");
+                    result = n.Remove(i, m.Groups[0].Length);
+                    result = result.Insert(i, "x");
+                    int left = Int32.Parse(m.Groups[1].ToString());
+                    int right = Int32.Parse(m.Groups[2].ToString());
+                    // Find right if it exists
+                    m = Regex.Match(result, @"x\D+?(\d+)");
+                    if (m.Success)
+                    {
+                        int val = Int32.Parse(m.Groups[1].ToString());
+                        val += right;
+                        result = result.Remove(m.Groups[1].Index, m.Groups[1].Length);
+                        result = result.Insert(m.Groups[1].Index, val.ToString());
+                    }
+                    // Find left if it exists
+                    m = Regex.Match(result, @"(\d+)\D+?x");
+                    if (m.Success)
+                    {
+                        int val = Int32.Parse(m.Groups[1].ToString());
+                        val += left;
+                        result = result.Remove(m.Groups[1].Index, m.Groups[1].Length);
+                        result = result.Insert(m.Groups[1].Index, val.ToString());
+                    }
+
+                    result = result.Replace('x', '0');
+                    return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+
+        private static bool Split(string n, out string result)
+        {
+            Match m = Regex.Match(n, @"(\d{2,})");
+            if (m.Success)
+            {
+                int val = Int32.Parse(m.Groups[0].ToString());
+                int left = val / 2;
+                int right = left + val % 2;
+                result = n.Remove(m.Groups[0].Index, m.Groups[0].Length);
+                result = result.Insert(m.Groups[0].Index, String.Format("[{0},{1}]", left, right));
                 return true;
             }
-
-            bool bExplode = false;
-            // traverse A branch
-            if (n.A != null)
+            else
             {
-                bExplode = Explode(n.A, depth + 1);
+                result = null;
+                return false;
             }
-            if (!bExplode &&
-                n.B != null)
-            {
-                bExplode = Explode(n.A, depth + 1);
-            }
-
-            return bExplode;
-        }
-
-        private static void AddLeft(Node n)
-        {
-            HashSet<Node> visited = new();
-            visited.Add(n);
-
-            Node current = n.Parent;
-            while (current != null)
-            {
-
-            }
-
-            return;
         }
     }
 
